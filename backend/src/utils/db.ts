d03db4681d9a7c4c07855b367f52ea02e46bc172
Ankit <ankityaduvanshi1122@gmail.com>
2026-04-09 23:34:28 +0530

    added postgres

--- backend/src/utils/db.ts
+++ backend/src/utils/db.ts
@@ -4,12 +4,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = new Pool({
-  user: process.env.DB_USER || 'ankit',
-  password: process.env.DB_PASSWORD || undefined,
-  host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT || '5432'),
-  database: process.env.DB_NAME || 'task_management',
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
+
+
 
 pool.on('error', (err) => {
   console.error('Unexpected error on idle client', err);
